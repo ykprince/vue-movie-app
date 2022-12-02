55adeabf91c956780b7d7a9a6a82361435cbf886
ykprince <songkruise@naver.com>
2022-12-02 17:51:48 +0900

    img commit

--- src/store/about.js
+++ src/store/about.js
@@ -5,6 +5,7 @@ export default {
       email: 'kruise1157@gmail.com',
       blog: 'https://nono.blog',
       phone: '+82-10-1234-5678',
-      image: 'https://heropy.blog/css/images/logo.png'
+    //   image: 'https://heropy.blog/css/images/logo.png'
+      image: 'https://raw.githubusercontent.com/ykprince/vue-movie-app/master/src/assets/logo.png'
     })
   }
